cf5c8018f0d09b7947b597b7ddd8f6fe20cfd9fb
= <=>
2026-04-16 21:23:55 +0300

    luotu komponenteille oma kansio

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function Navbar() {
+
+    return (
+        <>
+            <IconButton>
+                <MenuIcon fontSize='large' />
+            </IconButton>
+        </>
+    );
+
+
+}
