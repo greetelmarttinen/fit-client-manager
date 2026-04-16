d5989a3a2acd0fda54535be67294afeed8e89d1a
= <=>
2026-04-16 21:22:33 +0300

    tehty app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import Navbar from './components/NavBar';
+
 
 function App() {
-
-
   return (
-    <>
-
-    </>
+    <Container maxWidth="lg">
+      <AppBar position="static">
+        <Toolbar>
+          <Navbar />
+          <Typography variant="h6">Fit Client Manager</Typography>
+        </Toolbar>
+      </AppBar>
+      <CssBaseline />
+    </Container>
   )
 }
 
