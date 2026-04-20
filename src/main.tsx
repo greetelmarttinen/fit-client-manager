98e8bfcd76e3ae356d48856c15122a1c37ad5d3f
= <=>
2026-04-20 17:21:17 +0300

    lisätty createBrowserRouter -funktio toimivan navigoinnin luomiseksi

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import Customers from './components/Customers.jsx'
+import Trainings from './components/Trainings.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        path: "customers",
+        element: <Customers />
+      },
+      {
+        path: "trainings",
+        element: <Trainings />
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
