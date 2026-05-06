ad3c794244b68702920736cb1c310a4fc3290a6c
= <=>
2026-05-06 12:38:18 +0300

    Lisätty sisältötekstiä home -välilehdelle -> kertoo sovelluksesta

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,11 @@ import { Stack } from "@mui/material"
 export default function Home() {
     return (
         <Stack>
-            <h3>Welcome!</h3>
-        </Stack>
+            <h3>Personal Trainer Management App</h3>
+            <p>
+                This application is used to manage customers and their trainings.
+            </p>
+
+        </Stack >
     )
 }
