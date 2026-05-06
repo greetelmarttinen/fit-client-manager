639a67d73b789266f7dcb8bf90164858cf3fecd5
= <=>
2026-05-06 10:10:05 +0300

    Luotu Trainings -sivulle pohjaa/hahmotelmaa

--- src/components/Trainings.tsx
+++ src/components/Trainings.tsx
@@ -1,4 +1,8 @@
-import { Stack } from "@mui/material"
+import { useState, useEffect } from "react";
+import { Stack } from "@mui/material";
+import { DataGrid } from '@mui/x-data-grid';
+import { TrainingsDataType, CustomerDataType } from "../types";
+
 
 export default function Trainings() {
     return (
